0d8aaf2da37bc31da3651dc158236ac5a4746035
deloygintaras <turcinasgintaras@gmail.com>
2024-10-08 20:57:52 +0300

    funkcijos

---
+++ main.py
@@ -0,0 +1,70 @@
+# 1. Sukurkite funkciją, kuri išvestų jūsų vardą ir kodėl pasirinkote
+# programavimą. Iškvieskite šią funkciją tris kartus.
+# def vardas():
+#     print("Gintaras, pasirinkau nes noriu")
+# vardas()
+# vardas()
+# vardas()
+
+# 2. Sukurkite funkciją, kuri išvestų 5 eilučių eilėraštį. Iškvieskite šią funkciją 5
+# kartus.
+# def eilerastis():
+#     print("5 eiluciu eilerastis")
+# eilerastis()
+# eilerastis()
+# eilerastis()
+# eilerastis()
+# eilerastis()
+
+# 3. Sukurkite tris funkcijas, kur kiekviena išvestų skirtingus tekstus. Iškvieskite
+# visas tris funkcijas po vieną kartą.
+# def tekst():
+#     print("labas")
+# def tekst1():
+#     print("krabas")
+# def tekst2():
+#     print("kebabas")
+# tekst()
+# tekst1()
+# tekst2()
+
+
+
+# # 4. Sukurkite dvi funkcijas, kur vienoje būtų viena teksto eilutėje, kitoje kita.
+# # Sukurkite trečią funkciją, kuri iškviestų pirmas dvi funkcijas. Iškvieskite šią
+# # trečiąją funkciją už visų funkcijų ribų.
+# def f1():
+#     print("teksto eilute")
+# def f2():
+#     print("kita teksto eilute")
+# def f3():
+#     f1()
+#     f2()
+# f3()
+
+# 5. Sukurkite funkciją, kurios viduje sugeneruotumėte du atsitiktinius
+# skaičius. Funkcijoje suskaičiuokite ir išveskite šių dviejų skaičių sumą,
+# kartu išvedant ir patį atliekamą veiksmą (pvz 7 + 2 = 9). Iškvieskite šią
+# funkciją keletą kartų.
+
+
+
+
+
+# 6. Sukurkite ir iškvieskite funkciją, kurioje kintamuosiuose būtų saugoma
+# informacija apie policininką (vardas, pavardė, amžius, alga, etatas,
+# specializacija). Išveskite šią informaciją suformatuotai (pavyzdžiui įterpkite
+# į sakinį, ar išveskite sąrašu ar pan.).
+
+
+
+# 7. Sukurkite funkciją, kuri išvestų 10 atsitiktinių skaičių. Po visų išvestų
+# skaičių turėtų padėti tuščią eilutę. Šią funkciją iškvieskite 5 kartus.
+
+
+
+# 8. Sukurkite funkciją, kuri išvestų atsitiktinį skaičių. Už funkcijos ribų
+# sukurkite ciklą, kurį būtų vykdomas 10 kartų. Iškvieskite sukurtą funkciją
+# cikle. Turėtumėte pamatyti 10 atsitiktinių skaičių.
+
+
